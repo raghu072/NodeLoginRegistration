6833393a9c5b12655e8c936ecacd0e54c979814b
Raghavendra Purohit <raghavendra.purohit@TheoremIndia.net>
2017-08-07 12:24:19 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Put these statements before you define any routes.
+var parser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+/* Login */
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Login Page' });
+});
+
+/* Register */
+router.get('/register', function(req, res, next) {
+    res.render('register', { title: 'Register Page' });
+});
+
+/* Register users. */
+router.post('/register', parser, function(req, res, next) {
+    var nameForm = req.body.uname;
+    var usernameForm = req.body.username;
+    var emailForm = req.body.email;
+    var passwordForm = req.body.password;
+    var password2Form = req.body.password2;
+
+    console.log(nameForm);
+});
+
+module.exports = router;
